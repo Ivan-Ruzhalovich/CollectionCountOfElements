c0c28df978e068b94977593918fe25406706ece1
Ivan-Ruzhalovich <ruzhalovich@bk.ru>
2025-02-05 09:06:50 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+public class Main {
+    public static void main(String[] args) {
+        String[] mas =new String[]{"as","dsd","as"};
+        System.out.println(countOfElements(mas));
+    }
+
+    public static <T> Map<T,Integer> countOfElements(T[] mas){
+        Map<T,Integer> result = new HashMap<>();
+        for (T object:mas){
+            if (result.containsKey(object)){
+                result.replace(object,result.get(object)+1);
+            }
+            else result.put(object,1);
+        }
+        return result;
+    }
+}
